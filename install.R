99e1de6eb7ad3fd7d495ba93d0a5f03124797b3f
Nourelhouda96 <166914387+Nourelhouda96@users.noreply.github.com>
2025-12-13 21:40:27 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,18 @@
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages("leaflet")
+install.packages(c(
+  "plyr", "dplyr", "ggplot2", "formattable",
+  "multcomp", "vegan", "limma"
+))
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+BiocManager::install(c("phyloseq", "edgeR", "limma"))
+install.packages("remotes")
+remotes::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+install.packages("multcompView")
+Install speedyseq for faster phyloseq calculations
+install.packages("remotes")
+remotes::install_github("mikemc/speedyseq")
+install.packages("rstatix")
+install.packages("emmeans")
+install.packages("pkgbuild")
+pkgbuild::has_build_tools(debug = TRUE)
