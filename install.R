ecec697e575a7db60c568514334ef030203b0e75
Nourelhouda96 <166914387+Nourelhouda96@users.noreply.github.com>
2025-12-15 10:02:02 +0100

    Update install.R

--- install.R
+++ install.R
@@ -9,10 +9,14 @@ BiocManager::install(c("phyloseq", "edgeR", "limma"))
 install.packages("remotes")
 remotes::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
 install.packages("multcompView")
-Install speedyseq for faster phyloseq calculations
 install.packages("remotes")
 remotes::install_github("mikemc/speedyseq")
 install.packages("rstatix")
 install.packages("emmeans")
 install.packages("pkgbuild")
 pkgbuild::has_build_tools(debug = TRUE)
+install.packages(c(
+  "broom",
+  "Hmisc",
+  "ggpubr"
+))
