09f6ebe62344d33821641a81241065b140967a73
Nourelhouda96 <166914387+Nourelhouda96@users.noreply.github.com>
2025-12-15 20:54:07 +0100

    Update install.R

--- install.R
+++ install.R
@@ -6,8 +6,28 @@ install.packages("emmeans")
 install.packages("broom")
 install.packages("Hmisc")
 install.packages("ggpubr")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("vegan")
 
+# Bioconductor setup
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
 
+BiocManager::install(
+  c(
+    "phyloseq",
+    "Biostrings",
+    "limma",
+    "edgeR"
+  ),
+  ask = FALSE,
+  update = FALSE
+)
+
+# GitHub packages (AFTER phyloseq exists)
 install.packages("remotes")
 
 remotes::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
